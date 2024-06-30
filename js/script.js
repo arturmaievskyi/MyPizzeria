6f2b1a62396318fc07ca00e3298ff710309cf3cc
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-06-30 23:18:57 +0200

    added my slaves

--- js/script.js
+++ js/script.js
@@ -27,3 +27,38 @@ getProducts().then(function(products){
         })
     }
 })
+
+
+
+
+
+async function getSlaves (){
+    let response = await fetch("slaves.json")
+    let slaves = await response.json()
+    return slaves
+}
+
+
+function getSlavesHTML(slaves) {
+    let slavesData = JSON.stringify(slaves); 
+    return      `   <div class="card text-bg-dark" style="max-width: 25rem;">
+                        <img src="${slaves.image}" class="card-img" >
+                        <div class="card-img-overlay">
+                            <h5 class="card-title">${slaves.name}<h5>
+                            <p class="card-text ">${slaves.description}</p>
+                            <div>
+                                <p class="badge rounded-pill text-bg-warning">Work: ${product.work} euro</p>
+                            </div>
+                        </div>
+                    </div>`
+}
+
+
+getSlaves().then(function(products){
+    let productsList = document.querySelector('.Slaves')
+    if (productsList) { 
+        products.forEach(function(product) {
+            productsList.innerHTML += getCardHTML(product)
+        })
+    }
+})
