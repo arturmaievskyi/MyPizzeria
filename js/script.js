675d350e18b1362bcf086217a1565bee93ceb5a7
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-06-30 13:10:16 +0200

    changes script.js

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ async function getProducts (){
     let products = await response.json()
     return products
 }
+
+
 let products = await getProducts()
 console.log(products)
 
 
 
 function getCardHTML(product) {
-    let productData = JSON.stringify(product) // Створюємо JSON-рядок з даними про товар
+    let productData = JSON.stringify(product); 
     return      `<div class="my-card" style="">
-                    <img src="img/${product.image}">
+                    <img src="${product.image}">
                     <h5 class="text-my-card">${product.title}</h5>
                     <p class="description-card"> ${product.description} </p>
                     <button type="button" data-product='${productData}' class="cart-btn">Купити</button>
@@ -20,7 +22,7 @@ function getCardHTML(product) {
 
 
 getProducts().then(function(products){
-    let productsList = document.querySelector('.products-list')
+    let productsList = document.querySelector('.pizzas')
     if (productsList) { 
         products.forEach(function(product) {
             productsList.innerHTML += getCardHTML(product)
